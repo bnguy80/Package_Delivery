b89b455e0f4d3835243a49240a00fe3118bf8724
bnguy80 <80481071+bnguy80@users.noreply.github.com>
2023-08-11 16:57:50 -0400

    Decoupled algorithms

--- package_delivery/delivery/Trucks.py
+++ package_delivery/delivery/Trucks.py
@@ -1,5 +1,6 @@
-import math
 
+from package_delivery.algorithms.Nearest_Neighbor_Algo import sort_packages_on_truck
+from package_delivery.algorithms.Two_Opt_Route import two_opt_route
 from package_delivery.data_structures.AdjacencyMatrix import graph_access
 from package_delivery.algorithms.DijkstraAlgo import dijkstra
 from package_delivery.data_structures.HashMap import package_hashmap
@@ -59,18 +60,6 @@ class Trucks:
     def remove_packages(self, package):
         self.packages.remove(package)
         self.route.remove(package.address)
-
-    # Update package:
-
-    def update_package_in_truck(self, package_id, new_address, new_city, new_state, new_zipcode):
-        for package in self.packages:
-            if package['package_id'] == package_id:
-                package['address'] = new_address
-                package['city'] = new_city
-                package['state'] = new_state
-                package['zipcode'] = new_zipcode
-                return True
-        return False
 
     def insert_distances_pred_vertex(self, distance, pred_vertex):
         self.distances.append(distance)
@@ -218,15 +207,11 @@ class Trucks:
 # priority_queue to pop off packages on trucks until empty?
 # Once first truck is empty driver returns to hub to get next truck for deliveries
 def load_trucks(truck_high_priority, truck_medium_priority, truck_low_priority, graph, track_package_id):
-
-
     # Define the arguments as a single variable
     # Load high priority
     delivery_deadline1 = "9:00 AM"
     # packages besides 15, 14, 19, 16, 13, 20 share same package.address for some on loaded on truck
     constraint_list1 = [15, 14, 19, 16, 13, 20, 39, 21]
-    # Track status of packages and trucks from start of day to end of day
-    track_time1 = truck_high_priority.time_tracker
     args1 = {
         'trucks': truck_high_priority,
         'graph': graph,
@@ -241,14 +226,12 @@ def load_trucks(truck_high_priority, truck_medium_priority, truck_low_priority, 
     # Load medium priority
     delivery_deadline2 = "10:30 AM"
     constraint_list2 = "Can only be on truck 2"
-    track_time2 = truck_medium_priority.time_tracker
     args2 = {
         'trucks': truck_medium_priority,
         'graph': graph,
         'delivery_deadline': delivery_deadline2,
         'constraints': constraint_list2,
         'track_package_id': track_package_id,
-        # 'track_time': track_time2
     }
     load_packages(**args2)
     Trucks.set_edge_weight(truck_medium_priority)
@@ -256,7 +239,6 @@ def load_trucks(truck_high_priority, truck_medium_priority, truck_low_priority, 
     # Load low priority
     delivery_deadline3 = "EOD"
     constraint_list3 = None
-    track_time3 = truck_low_priority.time_tracker
 
     args3 = {
         'trucks': truck_low_priority,
@@ -264,7 +246,6 @@ def load_trucks(truck_high_priority, truck_medium_priority, truck_low_priority, 
         'delivery_deadline': delivery_deadline3,
         'constraints': constraint_list3,
         'track_package_id': track_package_id,
-        # 'track_time': track_time3
     }
     load_packages(**args3)
     Trucks.set_edge_weight(truck_low_priority)
@@ -356,43 +337,6 @@ def load_packages(trucks, graph, delivery_deadline, constraints, track_package_i
     # trucks.print_packages()
 
 
-# Implement the nearest neighbor algorithm,
-# sort the order of the packages loaded on Trucks object after loading the packages;
-# this is to reduce the total_distance traveled when calling the 'find_shortest_route_to_deliver'
-# function that where we implement dijkstra's algorithm
-# Will remove a number of packages with the same address from the packages sorted in trucks
-# This does not affect the process of delivering packages for all the packages
-def sort_packages_on_truck(trucks, graph):
-    # Start from the hub
-    current_vertex = '4001 South 700 East'
-    sorted_packages = []
-    sorted_route = []
-
-    # While there are still packages on the truck
-    while len(trucks.get_packages()) > 0:
-        min_distances = math.inf
-        nearest_package = None
-
-        # Find the nearest_package from the current_vertex
-        for package in trucks.get_packages():
-            dest_vertex = package.address
-            # Calculate distance between current_vertex and dest_vertex
-            distance = graph.edge_weight[current_vertex][dest_vertex]
-            if distance < min_distances:
-                min_distances = distance
-                nearest_package = package
-        # Move to the nearest package and mark it as loaded on the truck and its route
-        sorted_packages.append(nearest_package)
-        sorted_route.append(nearest_package.address)
-        current_vertex = nearest_package.address
-        trucks.remove_packages(nearest_package)
-    trucks.packages = sorted_packages
-    trucks.route = sorted_route
-    # print()
-    # print("SORTED_PACKAGES: ", sorted_packages)
-    # print()
-
-
 # Packages not loaded on all three Trucks object because packages < 16 on Trucks object
 # and will remain currently at hub, package_id add to the track_package_id set()
 # when loading packages, get left_over from non-present package.pacakge_id's
@@ -449,7 +393,7 @@ def find_shortest_route_to_deliver(truck, graph):
         # Vertex to travel to
         dest_vertex = package.address
         # Calculate the shortest distances and pred_vertex using Dijkstra's algorithm
-        distances1, pred_vertex1 = dijkstra(graph, start_vertex1)
+        distances1, pred_vertex1 = dijkstra(graph, start_vertex1, truck.route)
         # Make a copy of pred_vertex list to use for to find route back to hub
         pred_vertex_copy.append(pred_vertex1.copy())
         # Insert the calculated distances and pred_vertex into the Trucks object
@@ -475,7 +419,7 @@ def find_shortest_route_to_deliver(truck, graph):
     # Calculate the distance of the optimized route back to the hub
     for i in range(len(optimized_route_back_to_hub)):
         from_vertex = optimized_route_back_to_hub[i]
-        distances1, _ = dijkstra(graph_access, from_vertex)
+        distances1, _ = dijkstra(graph_access, from_vertex, truck.route)
         total = distances1[from_vertex]
         total_distance += total
     total_distance = round(total_distance, 2)
@@ -544,6 +488,8 @@ def deliver_packages(trucks, graph, start_interval, end_interval):
                 print("BACK_TO_HUB:", optimized_route_back_to_hub)
             # Only deliver packages if the current truck is truck1 or truck2
             if current_truck == high_priority or current_truck == medium_priority:
+                # Call the function two_opt_route to find the optimized route for the current truck
+                two_opt_route(current_truck, graph)
                 # Call the function to find the shortest route to deliver packages
                 total_distance = find_shortest_route_to_deliver(current_truck, graph)
                 # Update miles traveled for the current truck
@@ -598,7 +544,8 @@ def deliver_truck3_packages(truck3, graph, start_interval, end_interval):
         truck3.pred_vertex, truck3.route[-1], '4001 South 700 East'
     )
     print("BACK_TO_HUB:", optimized_route_back_to_hub)
-
+    # Call the function two_opt_route to find the optimized route for truck 3
+    two_opt_route(truck3, graph)
     # Call the function to find the shortest route to deliver packages
     total_distance = find_shortest_route_to_deliver(truck3, graph)
     # Update miles traveled for the current truck
@@ -611,98 +558,28 @@ def deliver_truck3_packages(truck3, graph, start_interval, end_interval):
     time_tracker.print_miles_traveled(truck3.truck_id)
 
 
-def two_opt_swap(route, i, j):
+# Find route back to hub from last package.address delivered
+def find_optimized_route_back_to_hub(predecessor_vertices, destination_vertex, hub_vertex):
     """
-    Reverses the order of vertices between indices i and j in the given route.
+    Find the optimized route back to the hub.
 
     Args:
-        route (list): A list of vertices representing a route.
-        i (int): The starting index of the vertices to be reversed.
-        j (int): The ending index of the vertices to be reversed.
+        predecessor_vertices (list): The list of predecessor vertices.
+        destination_vertex (str): The destination vertex.
+        hub_vertex (str): The hub vertex.
 
     Returns:
-        list: A new list of vertices with the order of vertices between indices i and j reversed.
+        list: The optimized route back to the hub.
     """
-    new_route = route[:i] + list(reversed(route[i:j + 1])) + route[j + 1:]
-    return new_route
-
-
-def calculate_route_distance(route, graph):
-    total_distance = 0
-    for i in range(len(route) - 1):
-        from_vertex = route[i]
-        to_vertex = route[i + 1]
-        total_distance += graph.edge_weight[from_vertex][to_vertex]
-    return total_distance
-
-
-# Remove repeated vertices in the route list
-def remove_repeated_vertices(route):
-    # Create new list with first vertex from current route
-    unique_route = [route[0]]
-    for vertex in route:
-        # If the current vertex is different from the last vertex added to the unique route,
-        # then add the current vertex to the unique route list
-        if vertex != unique_route[-1]:
-            unique_route.append(vertex)
-    return unique_route
-
-
-# Implement the two-opt algorithm, optimize the order of addresses in route in
-# conjunction to utilizing dijkstra's algorithm
-# Used to further decrease the total_distance traveled by the three trucks after nearest neighbor algorithm
-def two_opt_route(trucks, graph):
-    # Only unique address on the route list
-    unique_route = remove_repeated_vertices(trucks.route)
-    # Initialize the unique_route to the current_route
-    current_route = unique_route
-    # Will be best optimized route
-    best_route = current_route
-    improvement = True
-    # Continue to optimize until no further improvements are made
-    while improvement:
-        improvement = False
-        # Calculate distance of current_route
-        best_distance = calculate_route_distance(current_route, graph)
-        # Iterate through each vertex in the route except the first and last
-        for i in range(1, len(current_route) - 1):
-            for j in range(i + 1, len(current_route)):
-                # Create new route
-                new_route = two_opt_swap(current_route, i, j)
-                # Calculate distance of new_route
-                new_distance = calculate_route_distance(new_route, graph)
-                # If new_distance is improvement of current best_route then update
-                if new_distance < best_distance:
-                    best_route = new_route
-                    best_distance = new_distance
-                    improvement = True
-        # Set current_route to best_route in this iteration
-        current_route = best_route
-
-    # Update the truck's route with the optimized route
-    trucks.route = best_route
-
-    # Optimize the order of packages to reflect the optimized route
-    optimized_packages = []
-    for address in current_route:
-        for package in trucks.get_packages():
-            if package.address == address:
-                optimized_packages.append(package)
-                break
-    trucks.packages = optimized_packages
-
-
-# Find route back to hub from last package.address delivered
-def find_optimized_route_back_to_hub(pred_list, destination_vertex, hub_vertex):
     efficient_route = []
     current_vertex = destination_vertex
     while current_vertex != hub_vertex:
         efficient_route.append(current_vertex)
-        # No route to start_vertex, terminate the loop
-        if current_vertex not in pred_list:
+        # No route to hub_vertex, terminate the loop
+        if current_vertex not in predecessor_vertices:
             break
-        current_vertex = pred_list[current_vertex]
-    # In case the last element of pred_vertex is None, don't include it in the route
+        current_vertex = predecessor_vertices[current_vertex]
+    # In case the last element of efficient_route is None, don't include it in the route
     if efficient_route and efficient_route[-1] is None:
         efficient_route.pop()
     efficient_route.append(hub_vertex)
@@ -737,10 +614,6 @@ low_priority.time_tracker.initialize_multiple_package_status(low_priority.get_pa
 sort_packages_on_truck(high_priority, graph_access)
 sort_packages_on_truck(medium_priority, graph_access)
 sort_packages_on_truck(low_priority, graph_access)
-# # Two opt
-# two_opt_route(high_priority, graph_access)
-# two_opt_route(medium_priority, graph_access)
-# two_opt_route(low_priority, graph_access)
 
 truck_list = [high_priority, medium_priority, low_priority]
 deliver_packages(truck_list, graph_access, '8:35', '9:25')
