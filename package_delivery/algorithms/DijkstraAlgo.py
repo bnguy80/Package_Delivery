dbdefa1f522f6414a525aa254f72d3c0432f391b
bnguy80 <80481071+bnguy80@users.noreply.github.com>
2023-08-16 16:28:34 -0400

    No changes worth mentioning

--- package_delivery/algorithms/DijkstraAlgo.py
+++ package_delivery/algorithms/DijkstraAlgo.py
@@ -8,7 +8,7 @@ import math
 # find path to from start_vertex to end_vertex pred_vertex, whenever comparison to assign distance, append pred_vertex
 #
 def dijkstra(graph1, src, route):
-    distances: dict = {node: math.inf for node in route}  # Initialize distances of vertices in the route to infinity
+    distances: dict = {node: math.inf for node in route}  # Initialize all distances to infinity in route
     pred_vertex: dict = {node: None for node in graph1.vertices}  # Initialize predecessor vertices to None
     distances[src] = 0  # Set source vertex distance to 0
     # The min-heap is used to prioritize processing vertices with the shortest known distance first,
@@ -29,24 +29,24 @@ def dijkstra(graph1, src, route):
         if current_vertex in visited_queue and current_distance > distances[current_vertex]:
             continue
         visited_queue.append(current_vertex)  # Mark the vertex as visited
-        
+
         # Check if neighbor is in route before entering the loop
         neighbors = [neighbor for neighbor in graph1.edge_weight[current_vertex].keys() if neighbor in route]
         # Iterate over the neighbors of the current_vertex and their corresponding weights
         for neighbor in neighbors:
             weight = graph1.edge_weight[current_vertex][neighbor]
-            # Calculate the distance from the src vertex to this neighbor by adding the weight of the edge
-            # connecting them to the current_distance
-            distance = current_distance + weight
+            # Calculate the total distance to the neighbor by adding the weight of the edge
+            # between the current_vertex and the neighbor to the distance of the current_vertex.
+            total_distance = current_distance + weight
             # If the calculated distance is smaller than the current known distance to the neighbor,
             # update the distances dictionary with the new, smaller distance.
-            if distance < distances[neighbor]:
-                distances[neighbor] = distance
+            if total_distance < distances[neighbor]:
+                distances[neighbor] = total_distance
                 # Update the predecessor vertex for the 'neighbor' vertex to be the 'current_vertex'.
                 pred_vertex[neighbor] = current_vertex
                 # Push the updated distance and 'neighbor' vertex as a tuple into the min-heap.
                 # This ensures that the 'neighbor' vertex will be processed later with its updated distance.
-                heapq.heappush(min_heap, (distance, neighbor))
+                heapq.heappush(min_heap, (total_distance, neighbor))
     # Returns distances and pred_vertex dictionaries
     return distances, pred_vertex
 
