d7772350b370baf19cbce5819c767dbb3d388d16
bnguy80 <80481071+bnguy80@users.noreply.github.com>
2023-08-23 16:09:29 -0400

    Docstrings

--- package_delivery/data_structures/HashMap.py
+++ package_delivery/data_structures/HashMap.py
@@ -53,6 +53,13 @@ class HashMapEntry:
 
     # Overwrite print(HashMapEntry) otherwise it will print object reference, 7/14/23 working for now
     def __repr__(self):
+        """
+        Returns a string representation of the object.
+        This function concatenates the values of the package_id, address, city, state, zipcode, delivery_deadline, mass, and special_notes attributes, and returns them as a formatted string.
+
+        Returns:
+            str: A string representation of the object.
+        """
         return f'<{self.package_id} {self.address} {self.city} {self.state} {self.zipcode} {self.delivery_deadline} {self.mass} {self.special_notes}>'
 
 
@@ -66,11 +73,30 @@ class HashMap:
 
     # Hash function to generate bucket index of Hash Map, direct hashing package_id
     def _get_hash(self, key):
+        """
+        Calculate the hash value for the given key.
+
+        Parameters:
+            key (int): The key for which the hash value needs to be calculated.
+
+        Returns:
+            int: The calculated hash value.
+        """
         bucket = key % len(self.map)
         return bucket
 
     # Insert new packages into hash Map
     def insert_package(self, key, value):
+        """
+        Inserts a key-value pair into the hash table.
+
+        Parameters:
+            key (int): The key to be inserted into the hash table.
+            value (HashMapEntry): The corresponding value to be inserted.
+
+        Returns:
+            bool: True if the insertion was successful, False otherwise.
+        """
         bucket = self._get_hash(key)  # Get bucket
         bucket_list = self.map[bucket]  # Get bucket list where item will go
         key_value = [key, value]  # Key-entry pair
@@ -96,6 +122,15 @@ class HashMap:
 
     # Get value in key-value pair data from packaged_id of Hash Map
     def get_value_from_key(self, key):
+        """
+        Given a key, this function retrieves the corresponding value from the hash map.
+
+        Parameters:
+            key (int): The key to search for in the hash map.
+
+        Returns:
+            HashMapEntry: The value associated with the given key, or None if the key is not found.
+        """
         bucket = self._get_hash(key)
         bucket_list = self.map[bucket]
 
@@ -108,6 +143,15 @@ class HashMap:
             return None
 
     def get_address_from_key(self, key):
+        """
+        Retrieves the address associated with a given key from the hash map.
+
+        Args:
+            key (int): The key to search for in the hash map.
+
+        Returns:
+            Union[str, None]: The address associated with the key, or None if the key is not found.
+        """
         bucket = self._get_hash(key)
         bucket_list = self.map[bucket]
 
@@ -118,6 +162,15 @@ class HashMap:
             return None
 
     def get_key_from_address(self, address):
+        """
+        Returns the key associated with the given address in the map.
+
+        Parameters:
+            address (str): The address to search for in the map.
+
+        Returns:
+            int: The key associated with the given address, or None if the address is not found.
+        """
         for bucket in self.map:
             for key_value_pair in bucket:
                 if key_value_pair[1].address == address:
@@ -125,13 +178,22 @@ class HashMap:
         return None
 
     def get_hashmap(self):
+        """
+        Returns the hashmap associated with the object.
+        """
         return self.map
 
     def get_packages(self):
+        """
+        Returns the hashmap associated with the object.
+        """
         hashmap = self.get_hashmap()
         return hashmap
 
     def print_get_all_packages(self):
+        """
+        Prints all the packages stored in the map.
+        """
         print("All Packages:")
         print("-" * 40)
 
@@ -148,6 +210,16 @@ class HashMap:
 
     # To update value in key-value pair if changed
     def update_value(self, key, value):
+        """
+        Updates the value of a key in the map.
+
+        Parameters:
+            key (int): The key to update the value for.
+            value (HashMapEntry): The new value to update the key with.
+
+        Returns:
+            bool: True if the value was updated successfully, False otherwise.
+        """
         bucket = self._get_hash(key)
         bucket_list = self.map[bucket]
 
@@ -162,6 +234,15 @@ class HashMap:
 
     # Find key-value pair to delete
     def delete_key_value(self, key):
+        """
+        Deletes a key-value pair from the map based on the given key.
+
+        Parameters:
+            key (int): The key of the key-value pair to be deleted.
+
+        Returns:
+            bool: True if the key-value pair was successfully deleted, False otherwise.
+        """
         bucket = self._get_hash(key)
         bucket_list = self.map[bucket]
 
@@ -220,12 +301,31 @@ def load_hash_map(fileName):
 
 # Get full list of values printed, testing purposes for functions editing list
 def get_hash_map_all():
+    """
+    Get all values from the hash map.
+
+    This function iterates over the keys from 1 to 40 and prints the corresponding value
+    from the `package_hashmap` using the `get_value_from_key` method.
+
+    Returns:
+        None
+    """
     for i in range(1, 41):
         print(package_hashmap.get_value_from_key(i))
 
 
 # Check if all packages exist in HashMap object for testing purposes
 def check_all_packages(hashmap, total_packages=40):
+    """
+    Check if all packages are present in the hashmap.
+
+    Parameters:
+        hashmap (HashMap): A dictionary containing package IDs as keys and their values.
+        total_packages (int): The total number of packages to check.
+
+    Returns:
+        bool: True if all packages are present in the hashmap, False otherwise.
+    """
     for package_id in range(1, total_packages + 1):
         if hashmap.get_value_from_key(package_id) is None:
             return False
@@ -235,17 +335,3 @@ def check_all_packages(hashmap, total_packages=40):
 # Hash Map object instance to insert into HashMap
 package_hashmap = load_hash_map(r'C:\Users\brand\IdeaProjects\Package_Delivery_Program_New\package_delivery'
                                 r'\data_structures\WGUPS Package File Formatted.csv')
-
-
-# print(sorted(package_hashmap.get_packages(), key=lambda x: x[0], reverse=True))
-
-# all_packages_exist = check_all_packages(package_hashmap)
-# print("ALL_PACKAGES_EXIST: ", all_packages_exist)
-# get_hash_map_all() # Print HashMap correctly 7/19/23
-
-# print(package_hashmap) # Printing HashMap object 7/19/23
-
-# print(package_hashmap.get_address_from_key(40)) # Printing addresses from key correctly 7/19/23
-
-# print(package_hashmap.get_key_from_address('4300 S 1300 E')) # Printing package_id from address correctly 7/19/23
-# Print the current working directory
