d2690a7c10f3c38b64fb6ffefd70ddf89fc4c1cb
bnguy80 <80481071+bnguy80@users.noreply.github.com>
2023-09-02 18:00:18 -0400

    Modified singleton pattern, manage the different data states of trucks

--- package_delivery/delivery/logistics/fuel_tracker.py
+++ package_delivery/delivery/logistics/fuel_tracker.py
@@ -9,21 +9,35 @@ from pathlib import Path
 class FuelTracker:
     __FUEL_PRICE_CACHE = None  # To make sure the fuel price is only fetched once
 
-    def __init__(self, truck_id, truck_name, time_tracker):
-        # Consumption rate of each truck
-        self.__MPG = 18.0
-        # Fuel price per gallon fetched from API
-        if FuelTracker.__FUEL_PRICE_CACHE is None:
-            FuelTracker.__FUEL_PRICE_CACHE = self.fetch_fuel_price()
-        self.__FUEL_PRICE = FuelTracker.__FUEL_PRICE_CACHE
-        self.__TRAVEL_COST = 0
-        self.total_fuel_used = 0
-        # Track fuel usage, each truck has 30 gallons of fuel capacity
-        self.track_fuel_current_level = {truck_id: 30}
-        # Initialize full tank
-        self.__id = truck_id
-        self.__name = truck_name
-        self.__time_tracker = time_tracker
+    _instance = None  # Single instance storage
+    _initialized = False  # Initialization flag
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super(FuelTracker, cls).__new__(cls)
+        return cls._instance
+
+    def __init__(self, truck_id):
+        # Initialize the class attributes once
+        if not FuelTracker._initialized:
+            # Consumption rate of each truck
+            self.__MPG = 18.0
+            # Fuel price per gallon fetched from API
+            if FuelTracker.__FUEL_PRICE_CACHE is None:
+                FuelTracker.__FUEL_PRICE_CACHE = self.fetch_fuel_price()
+            self.__FUEL_PRICE = FuelTracker.__FUEL_PRICE_CACHE
+            self.__TRAVEL_COST = {}
+            self.total_fuel_used = {}
+            self.track_fuel_current_level = {}
+            FuelTracker._initialized = True
+
+        # Initialize full tank for each truck
+        if truck_id not in self.__TRAVEL_COST:
+            self.__TRAVEL_COST[truck_id] = 0
+        if truck_id not in self.total_fuel_used:
+            self.total_fuel_used[truck_id] = 0
+        if truck_id not in self.track_fuel_current_level:
+            self.track_fuel_current_level[truck_id] = 30
 
     @classmethod
     def fetch_fuel_price(cls):
@@ -36,13 +50,16 @@ class FuelTracker:
 
     @classmethod
     def fetch_fuel_price_csv(cls):
-        current_directory = Path(__file__).parent
-        csv_file_path = current_directory / 'utah_gas_prices.csv'
-        with open(csv_file_path, 'r') as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                return float(row['gasoline'])  # Working correctly 9/1/2023
-        return None
+        if cls.__FUEL_PRICE_CACHE is None:
+            current_directory = Path(__file__).parent
+            csv_file_path = current_directory / 'utah_gas_prices.csv'
+            with open(csv_file_path, 'r') as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    cls.__FUEL_PRICE_CACHE = row  # Save the data to cache
+                    return float(row['gasoline'])  # Working correctly 9/1/2023
+        else:
+            return float(cls.__FUEL_PRICE_CACHE['gasoline'])
 
     @classmethod
     def fetch_fuel_price_data(cls):
@@ -67,7 +84,11 @@ class FuelTracker:
             if item['name'] == 'Utah':
                 utah_data = item
                 break
-        # print("Utah data:", utah_data) # Debug line
+
+        # Debug line make sure API is being called
+        # have rate-limit 100(?)
+        print("Utah data:", utah_data)
+
         if utah_data:
             current_directory = Path(__file__).parent
             csv_file_path = current_directory / 'utah_gas_prices.csv'
@@ -85,34 +106,35 @@ class FuelTracker:
                     'diesel': utah_data['diesel']
                 })
 
-    def _calculate_fuel_used(self, miles_traveled):
+    def _calculate_fuel_used(self, truck_id, miles_traveled):
         fuel_used = miles_traveled / self.__MPG
-        self.total_fuel_used += fuel_used
+        self.total_fuel_used[truck_id] += fuel_used
         return fuel_used
 
-    def calculate_travel_cost(self):
-        cost = self.__FUEL_PRICE * self.total_fuel_used
+    def calculate_travel_cost(self, truck_id):
+        cost = self.__FUEL_PRICE * self.total_fuel_used[truck_id]
         return cost
 
-    def update_fuel_level(self):
-        miles_traveled = self.__time_tracker.get_miles_traveled()
-        fuel_used = self._calculate_fuel_used(miles_traveled)
-        self.track_fuel_current_level[self.__id] -= fuel_used
-        if self.track_fuel_current_level[self.__id] < 0:
+    def update_fuel_level(self, truck_id, time_tracker):
+        miles_traveled = time_tracker.get_miles_traveled()
+        fuel_used = self._calculate_fuel_used(truck_id, miles_traveled)
+        self.track_fuel_current_level[truck_id] -= fuel_used
+        if self.track_fuel_current_level[truck_id] < 0:
             print("Fuel level is empty!")
-            self.track_fuel_current_level[self.__id] = 0
-        self.__TRAVEL_COST = self.calculate_travel_cost()
+            self.track_fuel_current_level = 0
+        self.__TRAVEL_COST[truck_id] = self.calculate_travel_cost(truck_id)
 
-    def print_fuel_level(self):
-        print(f"{self.__name} FUEL_LEVEL:", self.track_fuel_current_level[self.__id], "gallons")
+    def print_fuel_level(self, truck_id):
+        print(f"{truck_id} FUEL_LEVEL:", self.track_fuel_current_level[truck_id], "gallons")
         print()
 
-    def print_fuel_used(self):
-        print(f"{self.__name} FUEL_USED:", self.total_fuel_used, "gallons")
+    def print_fuel_used(self, truck_id):
+        print(f"{truck_id} FUEL_USED:", self.total_fuel_used[truck_id], "gallons")
         print()
 
-    def print_fuel_price(self):
-        print(f"{self.__name} FUEL_PRICE:", self.__FUEL_PRICE)
+    def print_fuel_price(self, truck_id):
+        print(f"{truck_id} FUEL_PRICE:", self.__FUEL_PRICE)
 
-    def print_travel_cost(self):
-        print(f"{self.__name} TRAVEL_COST:", "$", int(self.__TRAVEL_COST), "dollars")
+    def print_travel_cost(self, truck_id):
+
+        print(f"{truck_id} TRAVEL_COST:", "$", int(self.__TRAVEL_COST[truck_id]), "dollars")
--- package_delivery/visualization/visualize.py
+++ package_delivery/visualization/visualize.py
@@ -2,59 +2,72 @@ import matplotlib.pyplot as plt
 
 
 class Visualize:
+    _instance = None  # Single instance storage
+    _initialized = False  # Initialization flag
     COLORS = ['red', 'blue', 'green']  # High Priority, Medium Priority, Low Priority
     LINE_STYLES = ['-', '--', ':']  # Solid, Dashed, Dotted line styles
     MARKERS = ['o', 's', '^']  # Circle, Square, Triangle markers
 
-    def __init__(self, addresses, truck_id, truck_name):
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super(Visualize, cls).__new__(cls)
+        return cls._instance
+
+    def __init__(self, truck_id):
         """
         Initialize the Visualize object
         """
-        self.__IMAGE_PATH = ('C:/Users/brand/IdeaProjects/Package_Delivery_Program_New/package_delivery/visualization'
-                             '/Picture1.jpg')
-        self.route = {}
-        self.addresses = addresses
-        self.truck_id = truck_id
-        self.truck_name = truck_name
-        # All coordinates (x, y) dictionary; visualize a Picture1.jpg dimensions 672x756
-        self.__ALL_COORDINATES = {
-            '4001 South 700 East': (397, 457),
-            '195 W Oakland Ave': (309, 316),
-            '2530 S 500 E': (372, 320),
-            '233 Canyon Rd': (344, 57),
-            '380 W 2880 S': (290, 351),
-            '410 S State St': (336, 122),
-            '3060 Lester St': (171, 369),
-            '300 State St': (335, 111),
-            '600 E 900 South': (413, 139),
-            '2600 Taylorsville Blvd': (110, 593),
-            '3575 W Valley Central Station bus Loop': (67, 478),
-            '2010 W 500 S': (131, 129),
-            '4580 S 2300 E': (550, 517),
-            '3148 S 1100 W': (216, 376),
-            '1488 4800 S': (184, 547),
-            '177 W Price Ave': (310, 424),
-            '3595 Main St': (330, 420),
-            '6351 South 900 East': (422, 678),
-            '5100 South 2700 West': (102, 572),
-            '5025 State St': (339, 557),
-            '5383 South 900 East #104': (413, 594),
-            '1060 Dalton Ave S': (213, 186),
-            '2835 Main St': (327, 345),
-            '1330 2100 S': (469, 288),
-            '3365 S 900 W': (239, 397),
-            '2300 Parkway Blvd': (130, 325),
-            '4300 S 1300 E': (447, 490)
-        }
-        # Create a figure and axes
-        self.fig, self.ax = None, None
-        # Create a scatter plot
-        self.scatter = None
-        # Create a line plot
-        self.line = None
+        if not Visualize._initialized:
+            self.__IMAGE_PATH = (
+                'C:/Users/brand/IdeaProjects/Package_Delivery_Program_New/package_delivery/visualization'
+                '/Picture1.jpg')
+            # Create a figure and axes
+            self.fig, self.ax = None, None
+            # Create a scatter plot
+            self.scatter = None
+            # Create a line plot
+            self.line = None
+            self.route = {}
+            self.address = {}
+            # All coordinates (x, y) dictionary; visualize a Picture1.jpg dimensions 672x756
+            self.__ALL_COORDINATES = {
+                '4001 South 700 East': (397, 457),
+                '195 W Oakland Ave': (309, 316),
+                '2530 S 500 E': (372, 320),
+                '233 Canyon Rd': (344, 57),
+                '380 W 2880 S': (290, 351),
+                '410 S State St': (336, 122),
+                '3060 Lester St': (171, 369),
+                '300 State St': (335, 111),
+                '600 E 900 South': (413, 139),
+                '2600 Taylorsville Blvd': (110, 593),
+                '3575 W Valley Central Station bus Loop': (67, 478),
+                '2010 W 500 S': (131, 129),
+                '4580 S 2300 E': (550, 517),
+                '3148 S 1100 W': (216, 376),
+                '1488 4800 S': (184, 547),
+                '177 W Price Ave': (310, 424),
+                '3595 Main St': (330, 420),
+                '6351 South 900 East': (422, 678),
+                '5100 South 2700 West': (102, 572),
+                '5025 State St': (339, 557),
+                '5383 South 900 East #104': (413, 594),
+                '1060 Dalton Ave S': (213, 186),
+                '2835 Main St': (327, 345),
+                '1330 2100 S': (469, 288),
+                '3365 S 900 W': (239, 397),
+                '2300 Parkway Blvd': (130, 325),
+                '4300 S 1300 E': (447, 490)
+            }
+            Visualize._initialized = True
 
-    def _setup_figure(self):
-        idx = (self.truck_id - 1) % 3
+        if truck_id not in self.route:
+            self.route[truck_id] = {}
+        if truck_id not in self.address:
+            self.address[truck_id] = {}
+
+    def _setup_figure(self, truck_id):
+        idx = (truck_id - 1) % 3
         # Read the image
         image = plt.imread(self.__IMAGE_PATH)
         # Create a figure with the original dimensions
@@ -69,41 +82,48 @@ class Visualize:
         self.line, = self.ax.plot([], [], color=self.COLORS[idx], linestyle=self.LINE_STYLES[idx], linewidth=2)
 
     # Get all coordinates from the addresses
-    def _populate_route_dict(self):
+    def _populate_route_dict(self, truck_id):
         """
         Populates the route dictionary with the addresses and their corresponding coordinates.
 
         Parameters:
-        - None
+        - truck_id (int): The truck ID.
 
         Returns:
         - None
         """
-        for address in self.addresses:
+        for address in self.address[truck_id]:
             if address in self.__ALL_COORDINATES:
-                self.route[address] = self.__ALL_COORDINATES[address]
+                self.route[truck_id][address] = self.__ALL_COORDINATES[address]
+
+    def populate_address(self, truck_id, address):
+        """
+        Populate the address attribute of the object.
+        """
+        self.address[truck_id] = address
 
     # Update after two_opt_swap and load_packages_nearest_neighbor used
-    def update_address(self, new_addresses):
+    def update_address(self, new_addresses, truck_id):
         """
         Update the address attribute of the object.
 
         Args:
-            new_addresses (str): The new addresses to be assigned to the address attribute.
+            new_addresses (any): The new addresses to be assigned to the address attribute.
+            truck_id (int): The truck ID.
 
         Returns:
             None
         """
-        self.addresses = new_addresses
+        self.address[truck_id] = new_addresses
 
-    def _get_coord_and_close_route(self):
+    def _get_coord_and_close_route(self, truck_id):
         """
         Get the coordinates and close the route.
 
         Returns:
             Tuple: The x and y coordinates of the route.
         """
-        x, y = zip(*self.route.values())
+        x, y = zip(*self.route[truck_id].values())
         # Close the route by adding the first and last coordinates
         if x[0] != x[-1] or y[0] != y[-1]:
             x = x + (x[0],)
@@ -111,34 +131,34 @@ class Visualize:
         return x, y
 
     # Visualize package locations
-    def visualize_package_locations(self):
+    def visualize_package_locations(self, truck_id, truck_name):
         # Set up the figure in a clean state
-        self._setup_figure()
+        self._setup_figure(truck_id)
         # Overlay markers on the visualization
-        self._populate_route_dict()
+        self._populate_route_dict(truck_id)
         # Get all the coordinates
-        all_points = [(x, y) for x, y in self.route.values()]
+        all_points = [(x, y) for x, y in self.route[truck_id]]
         # Update the scatter plot with the new coordinates
         self.scatter.set_offsets(all_points)
-        plt.title(f"{self.truck_name}, Package Locations")
+        plt.title(f"{truck_name}, Package Locations")
         # Display the visualization with markers using SciView
         plt.show()
 
         # Clears the route for the next visualization
         self.route = {}
 
-    def visualize_truck_route(self):
+    def visualize_truck_route(self, truck_id, truck_name):
         # Set up the figure in a clean state
-        self._setup_figure()
+        self._setup_figure(truck_id)
 
-        self._populate_route_dict()
+        self._populate_route_dict(truck_id)
 
-        x, y = self._get_coord_and_close_route()
+        x, y = self._get_coord_and_close_route(truck_id)
 
         # Update the line plot
         self.line.set_data(x, y)
 
-        plt.title(f'{self.truck_name} Truck Route')
+        plt.title(f'{truck_name} Truck Route')
 
         # Re-render the figure
         self.fig.canvas.draw_idle()
@@ -147,11 +167,11 @@ class Visualize:
         plt.close(self.fig)
 
         # Clears the route for the next visualization
-        self.route = {}
+        self.route[truck_id] = {}
 
-    def visualize_all_truck_routes(self, trucks_list):
+    def visualize_all_truck_routes(self, trucks_list, truck_id):
         # Set up the figure in a clean state
-        self._setup_figure()
+        self._setup_figure(truck_id)
         legend_labels = []
         line_objects = []
 
@@ -165,9 +185,9 @@ class Visualize:
 
         for idx, route in enumerate(all_routes):
             # Reset the route for each truck
-            self.route = route
+            self.route[truck_id] = route
 
-            x, y = self._get_coord_and_close_route()
+            x, y = self._get_coord_and_close_route(truck_id)
 
             # Update the line plot
             line, = self.ax.plot(x, y, color=self.COLORS[idx], linestyle=self.LINE_STYLES[idx],
@@ -186,9 +206,9 @@ class Visualize:
         # Clears the figure to make sure start fresh for the next visualization
         plt.close(self.fig)
         # Reset the route for the next visualization
-        self.route = {}
+        self.route[truck_id] = {}
 
-    def visualize_pie_chart(self, filtered_packages):
+    def visualize_pie_chart(self, filtered_packages, truck_name):
         # Status count of packages
         status_count = {'IN_TRANSIT': 0, 'AT_HUB': 0, 'DELIVERED': 0}
         for package, status_info in filtered_packages:
@@ -203,5 +223,5 @@ class Visualize:
         explode = tuple(0.1 for _ in range(len(labels)))  # Dynamically create explode tuple based on length of labels
         plt.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=90)
         plt.axis('equal')
-        plt.title(f'{self.truck_name} Package Status Distribution')
+        plt.title(f'{truck_name} Package Status Distribution')
         plt.show()
